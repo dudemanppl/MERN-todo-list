054f2f323d0630464ad9ebc23e0bf8a367c17640
Tommy Liao <tommyliao340@hotmail.com>
2019-03-22 22:26:19 -0700

    [feat] Completes step 1a

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, err => {
+  if (err) console.log(err);
+  console.log(`Connected to Express on port ${port}`);
+});
